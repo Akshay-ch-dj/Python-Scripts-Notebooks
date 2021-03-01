0b8ae3eb3bbf9b896ee14362671b14452684dfa8
Akshaychdev <akshaych.dev@gmail.com>
2021-03-01 21:41:56 +0530

    test commit for checking key working

--- sample.py
+++ sample.py
@@ -1,16 +1,1 @@
-def gridTraveller(m, n, memo={}):
-    # Check if the tuple-(m,n) exists in the dictionary as key
-    # if yes return the value mapped to it
-    if (m, n) in memo:
-        return memo[(m, n)]
-
-    if (m == 1 and n == 1):
-        return 1
-    if (m == 0 or n == 0):
-        return 0
-    memo[(m, n)] = gridTraveller(m-1, n, memo) + gridTraveller(m, n-1, memo)
-    return memo[(m, n)]
-
-
-# print(gridTraveller(3, 3))
-print(gridTraveller(18, 18))
+# making commit work or not
